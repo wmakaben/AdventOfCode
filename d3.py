d0f6838f0cf4d4861ac8ee22fa3f37179e5d0f2b
William Makabenta <wmakaben@gmail.com>
2018-12-03 07:35:01 -0500

    day 3, part 2

--- d3.py
+++ d3.py
@@ -34,4 +34,20 @@ print count
 
 # Part 2
 
-# file = open("input/d3.txt", "r")
+file = open("input/d3.txt", 'r')
+# Yep, brute force forever
+for line in file.read().splitlines():
+	input = line.split(" ")
+	offset = parseOffset(input[2])
+	dim = parseDim(input[3])
+
+	clean = True
+	for i in range(offset[0], offset[0] + dim[0]):
+		for j in range(offset[1], offset[1] + dim[1]):
+			if fabric[i][j] > 1:
+				clean = False
+				break
+
+	if clean:
+		print input
+
