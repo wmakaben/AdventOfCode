4ee8705b45e1a4daf263103f1b0fb012fddb9704
William Makabenta <wmakaben@gmail.com>
2018-12-05 07:36:51 -0500

    day 5, part 2

--- d5.py
+++ d5.py
@@ -7,15 +7,26 @@ def getOppCase (c):
 	else:
 		return c.upper()
 
-file = open("input/d5.txt", 'r')
-# file = open("input/d5-ex.txt", 'r')
-line = file.readline()
+def react (p):
+	for i in range(len(p) - 2, -1, -1):
+		if i+1 < len(p) and p[i] == getOppCase(p[i+1]):
+			p = p[:i] + p[i+2:]
+	return len(p)
 
+# file = open("input/d5-ex.txt", 'r')
+file = open("input/d5.txt", 'r')
+line = file.readline()
+print react(line)
+
+# Part 2
+print "Part 2"
+
+charSet = set(line.lower())
+length = len(line)
 # print line
-for i in range(len(line) - 2, -1, -1):
-	# print line[i]
-	if line[i] == getOppCase(line[i+1]):
-		line = line[:i] + line[i+2:]
-		# print line
-# print line
-print len(line)
+for c in charSet:
+	# print c
+	tmpLine = line.replace(c, "").replace(c.upper(), "")
+	# print tmpLine
+	length = min(length, react(tmpLine))
+print length
