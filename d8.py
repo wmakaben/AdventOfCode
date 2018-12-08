735364ce0e0c53929721e26fdab8753d9e9be449
William Makabenta <wmakaben@gmail.com>
2018-12-08 09:34:31 -0500

    day 8, part 2

--- d8.py
+++ d8.py
@@ -10,5 +10,29 @@ def treeSum(tree, sum):
 		sum += int(tree.pop(0))
 	return sum
 
+# Part 1
 tree = file.readline().split(" ")
-print(treeSum(tree, 0))
+# print(treeSum(tree, 0))
+
+# Part 2
+
+def rootVal(tree):
+	childrenNum = int(tree.pop(0))
+	metadataNum = int(tree.pop(0))
+	childrenVal = {}
+
+	for i in range(childrenNum):
+		childrenVal[i+1] = rootVal(tree)
+
+	val = 0
+	if childrenNum:
+		for i in range(metadataNum):
+			cIdx = int(tree.pop(0))
+			if cIdx in childrenVal:
+				val += childrenVal[cIdx]
+	else:
+		for i in range(metadataNum):
+			val += int(tree.pop(0))
+	return val
+
+print rootVal(tree)
