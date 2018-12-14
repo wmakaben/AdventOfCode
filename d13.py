22f67bfe9f40816396632bb7d5952ebf1b684311
William Makabenta <wmakaben@gmail.com>
2018-12-14 00:22:16 -0500

    day 13, part 1

--- d13.py
+++ d13.py
@@ -1,5 +1,8 @@
-file = open("input/d13-ex.txt", 'r')
-# file = open("input/d13.txt", 'r')
+import numpy as np
+
+# file = open("input/d13-ex.txt", 'r')
+# file = open("input/d13-ex-2.txt", 'r')
+file = open("input/d13.txt", 'r')
 
 '''
 left = -1
@@ -14,78 +17,64 @@ intVal change at intersection
 intVal = ((intVal + 2) % 3) - 1
 '''
 
-dirVal = { 0: '^', 1: '>', 2: 'v', 3: '<' }
-dirSymbol = { '^': 0, '>': 1, 'v': 2, '<': 3 }
 initTrack = { '>': '-', '<': '-', 'v': '|', '^': '|' }
-dirChange = {
-	'>': [1, 0],
-	'v': [0, 1],
-	'<': [-1, 0],
-	'^': [0, -1]
-}
-trackVal = {
-	'|': [0, 1],
-	'-': [1, 0]
-	# Get default is [1, 1]
-}
-dirSwitch = {
-	'/': { '>': '^', '^': '>', '<': 'v', 'v': '<' },
-	'\\': { '>': 'v', 'v': '>', '<': '^', '^': '<' },
+initDir = { '^': 0, '>': 1, 'v': 2, '<': 3 }
+# 		   ^				  >					v 				  <
+dirMove = [np.array([0, -1]), np.array([1, 0]), np.array([0, 1]), np.array([-1, 0])]
+trackMove = {'|': np.array([0, 1]), '-': np.array([1, 0])}
+cornerDirections = {
+	# 0 - 1, 2 - 3
+	# '/': { '>': '^', '^': '>', '<': 'v', 'v': '<' },
+	'/': [1,0,3,2],
+	# 1 - 2, 3 - 0
+	# '\\': { '>': 'v', 'v': '>', '<': '^', '^': '<' }
+	'\\': [3,2,1,0]
 }
 
-def getIntChange (symbol, change):
-	newSymbol = dirVal[(dirSymbol[symbol] + change) % 4]
-	newChange = ((change + 2) % 4) - 1
-	return [newSymbol, newChange]
+
+def init (lines, carts, tracks):
+	y = 0
+	for line in lines:
+		x = 0
+		row = []
+		for c in line.rstrip():
+			row.append(initTrack.get(c, c))
+			if c in initDir:
+				carts[str([x,y])] = [np.array([x,y]), initDir[c], -1]
+			x += 1
+		tracks.append(row)
+		y += 1
+
+def moveCart (cart, track):
+	cart[0] += (dirMove[cart[1]] * trackMove.get(track, np.array([1,1])))
+
+def turnCart (cart, track):
+	if track == '+':
+		cart[1] = (cart[1] + cart[2]) % 4
+		cart[2] = ((cart[2] + 2) % 3) - 1
+	elif track in cornerDirections:
+		cart[1] = cornerDirections[track][cart[1]]
 
 carts = {}
 tracks = []
-
-y = 0
-for line in file:
-	x = 0
-	trackRow = []
-	for c in line.rstrip():
-		trackRow.append(initTrack.get(c, c))
-		if c in initTrack:
-			carts[str([x,y])] = [x, y, c, -1]
-		x += 1
-	tracks.append(trackRow)
-	# print ''.join(trackRow)
-	y += 1
-
-# print carts
+init(file, carts, tracks)
 
 crash = False
 while not crash:
-	newCarts = {}
-	cartKeys = carts.keys()
-	cartKeys.sort()
-	for cartKey in cartKeys:
+	for cartKey in sorted(carts):
 		cart = carts[cartKey]
-		# Update position
-		dVal = dirChange[cart[2]]
-		tSymbol = tracks[cart[1]][cart[0]]
-		tVal = trackVal.get(tSymbol, [1,1])
-		newPos = [ cart[0] + (dVal[0] * tVal[0]), cart[1] + (dVal[1] * tVal[1]) ]
-		# Look for crash
-		newKey = str(newPos)
-		if newKey in carts or newKey in newCarts:
+		track = tracks[cart[0][1]][cart[0][0]]
+		moveCart(cart, track)
+		newCartKey = str(cart[0])
+
+		if newCartKey in carts:
 			crash = True
-			print newKey
+			print newCartKey
 			break
-		# Update direction
-		d = cart[2:]
-		tSymbol = tracks[newPos[1]][newPos[0]]
-		if tSymbol not in trackVal:
-			# print newKey + ', ' + tSymbol
-			if tSymbol in dirSwitch:
-				d[0] = dirSwitch[tSymbol][d[0]]
-			else:
-				d = getIntChange(d[0], d[1])
-		# print d
-		newCarts[newKey] = [newPos[0], newPos[1]] + d
-		carts.pop(cartKey)
-	carts = newCarts
-	print carts
+
+		track = tracks[cart[0][1]][cart[0][0]]
+		turnCart(cart, track)
+		carts[newCartKey] = carts.pop(cartKey)
+
+
 
