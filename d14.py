b6423e4c5b585ab6fb045d5002f974d2df415cee
William Makabenta <wmakaben@gmail.com>
2018-12-14 19:20:05 -0500

    day 14 part 2 - still somewhat manageable

--- d14.py
+++ d14.py
@@ -7,17 +7,25 @@ after 846601
 '''
 
 n = 846601
-
-recipes = [3, 7]
+# n = 5
+# recipes = [3, 7]
+recipes = '37'
 e1 = 0
 e2 = 1
 
-while len(recipes) < n + 10:
-	recipes += map(int, list(str(recipes[e1] + recipes[e2])))
-	e1 = (e1 + 1 + recipes[e1]) % len(recipes)
-	e2 = (e2 + 1 + recipes[e2]) % len(recipes)
+# while len(recipes) < n + 10:
+while str(n) not in recipes[-7:]:
+	# recipes += map(int, list(str(recipes[e1] + recipes[e2])))
+	# e1 = (e1 + 1 + recipes[e1]) % len(recipes)
+	# e2 = (e2 + 1 + recipes[e2]) % len(recipes)
+	recipes += str(int(recipes[e1]) + int(recipes[e2]))
+	e1 = (e1 + 1 + int(recipes[e1])) % len(recipes)
+	e2 = (e2 + 1 + int(recipes[e2])) % len(recipes)
 
-print ''.join(map(str, recipes[n:n+10]))
+# print ''.join(map(str, recipes[n:n+10]))
+# print ''.join(recipes[n:n+10])
 
+# print len(recipes)
+# print recipes[-7:]
 
-# print ''.join(map(str, recipes)).index(str(n))
+print len(recipes) - 7 + recipes[-7:].index(str(n))
