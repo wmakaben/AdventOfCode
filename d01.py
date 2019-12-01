215583b21a8881c86df0295f389a78bb52e80e5e
William Makabenta <wmakaben@gmail.com>
2019-12-01 09:32:12 -0500

    Day 1, Part 2

--- d01.py
+++ d01.py
@@ -7,10 +7,24 @@ totalFuel = 0
 for mass in file:
 	totalFuel = totalFuel + getFuel(int(mass))
 
-# Test Case: 654, 966
+# Test Case: 654
 # totalFuel = getFuel(1969)
-# Test Case: 33583, 50346
+# Test Case: 33583
 # totalFuel = getFuel(100756)
 
 print(totalFuel)
 # 3231941
+
+# Part 2
+
+file = open("input/d01.txt", 'r')
+
+totalFuel = 0
+for mass in file:
+	fuel = getFuel(int(mass))
+	while fuel > 0:
+		totalFuel += fuel
+		fuel = getFuel(fuel)
+
+print(totalFuel)
+# 4845049
