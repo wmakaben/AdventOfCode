d7ba14b3db71bb14838a87b3c0ad08bf78ae3943
William Makabenta <wmakaben@gmail.com>
2019-12-04 07:19:23 -0500

    Day 4, Part 2

--- d04.py
+++ d04.py
@@ -1,5 +1,8 @@
 r = [145852, 616942]
 # r = [145852, 145900]
+# r = [146000, 147000]
+# r = [114567, 114567]
+
 
 test = [11, 111, 1111, 11111, 111111]
 
@@ -13,18 +16,26 @@ while password <= r[1]:
 	hasDouble = False
 	updated = False
 	inc = 1
-	lastDigit = -1
+	lastDigit = get_digit(password, 0)
+	digitCount = 1
 	while i < 6:
 		digit = get_digit(password, i)
 		subPass = password % (10 ** (i + 1))
 		n = digit * test[i - 1]
-
+		
 		if n > subPass:
 			inc = n - subPass
 			hasDouble = False
-			i = 7
-		elif digit == lastDigit or (i == 1 and n == subPass):
-			hasDouble = True
+			i = 11
+		elif digit == lastDigit:
+			digitCount += 1
+			if i == 5 and digitCount == 2:
+				hasDouble = True
+		else:
+			if digitCount == 2:
+				hasDouble = True
+			digitCount = 1
+
 		lastDigit = digit
 		i += 1
 
