9bfe0edd7315048ccee6a90dffbecf8ec5d8b7cb
William Makabenta <wmakaben@gmail.com>
2019-12-07 09:39:50 -0500

    Day 7, Part 2

--- d07.py
+++ d07.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 file = open("input/d07.txt", 'r')
 intcode = list(map(int, file.readline().split(",")))
 
@@ -44,11 +46,10 @@ def getJumpParams(code, i):
 		p2 = code[code[i+2]]
 	return (p1,p2)
 
-def runProg (icode, params):
-	code = icode.copy()
+def runProg (code, params, idx):
 	paramCount = 0
 	outputSignal = None
-	i = 0
+	i = idx[0]
 	while True:
 		op = code[i]	
 		if op in exitOp:
@@ -62,7 +63,8 @@ def runProg (icode, params):
 			code[p[2]] = p[0] * p[1]
 			i += 4
 		elif op in inOps:
-			# print("input: ")
+			if paramCount >= len(params):
+				break
 			val = params[paramCount]
 			paramCount += 1
 			# val = input()
@@ -70,8 +72,8 @@ def runProg (icode, params):
 			code[p] = int(val)
 			i += 2
 		elif op in outOps:
+			# Assumes this only outputs once per amplifier "session"
 			p = getInParams(code, i)
-			# print(code[p])
 			outputSignal = code[p]
 			i += 2
 		elif op in jtOps:
@@ -88,27 +90,61 @@ def runProg (icode, params):
 			p = getMathParams(code, i)
 			code[p[2]] = 1 if p[0] == p[1] else 0
 			i += 4
-	# return code[0]
+	idx[0] = i
 	return outputSignal
 
+def getSignal(settings):
+	codeState = {}
+	codeIdx = {}
+	signal = None
+	currentSignal = 0
+
+	sIdx = 0
+	while currentSignal != None:
+		setting = settings[sIdx]
+		params = []
+		if setting not in codeState:
+			codeState[setting] = intcode.copy()
+			codeIdx[setting] = [0]
+			params.append(setting)
+		params.append(currentSignal)
+		currentSignal = runProg(codeState[setting], params, codeIdx[setting])
+
+		if currentSignal == None:
+			break
+
+		sIdx += 1
+		if sIdx >= len(settings):
+			signal = currentSignal
+			sIdx = sIdx % len(settings)
+
+	# for setting in settings:
+	# 	if setting not in codeState:
+	# 		codeState[setting] = intcode.copy()
+	# 		codeIdx[setting] = 0
+	# 	currentSignal = runProg(codeState[setting], [setting, currentSignal], codeIdx[setting])
+	return signal
+
+# Part 1 Tests:
 # intcode = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
 # settings = [4,3,2,1,0]
 # intcode = [3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0]
 # settings = [0,1,2,3,4]
 # intcode = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
 # settings = [1,0,4,3,2]
+# print(getSignal(settings))
 
-def getSignal(settings):
-	signal = 0
-	for setting in settings:
-		signal = runProg(intcode, [setting, signal])
-	return signal
+# Part 1 Solution:
+# settingsOptions = list(permutations(range(0,5)))
+# print(max(getSignal(s) for s in settingsOptions))
 
-from itertools import permutations
-settingsOptions = list(permutations(range(0,5)))
-print(len(settingsOptions))
+# Part 2 Tests:
+# intcode = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+# settings = [9,8,7,6,5]
+# intcode = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
+# settings = [9,7,8,5,6]
+# print(getSignal(settings))
 
+# Part 2 Solution:
+settingsOptions = list(permutations(range(5,10)))
 print(max(getSignal(s) for s in settingsOptions))
-
-
-
