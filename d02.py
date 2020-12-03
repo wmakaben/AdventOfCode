f4028b1e19816c86df89a4744371919f9094f185
= <wmakabenta@Wills-MacBook-Pro.local>
2020-12-02 21:33:49 -0500

    Day 2, Part 2

--- d02.py
+++ d02.py
@@ -1,5 +1,12 @@
 file = open("input/d02.txt", 'r')
 
+def isValidCount (rule, letter, code):
+    charCount = code.count(letter)
+    return charCount >= rule[0] and charCount <= r
+    
+def isValidPosition (rule, letter, code):
+    return (code[rule[0]-1] == letter or code[rule[1]-1] == letter) and code[rule[0]-1] != code[rule[1]-1]
+
 valid = 0
 for line in file:
     l = line.rstrip().split(' ')
@@ -7,8 +14,10 @@ for line in file:
     letter = l[1][0]
     code = l[2]
 
-    charCount = code.count(letter)
-    if charCount >= rule[0] and charCount <= rule[1]:
+    # if isValidCount(rule, letter, code):
+    #     valid += 1
+    
+    if isValidPosition(rule, letter, code):
         valid += 1
 
 print(valid)
