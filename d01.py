ecf0c3236a02ceffdc732c97cafe489e47fbd58f
Will Makabenta <wmakabenta@Wills-MacBook-Pro.local>
2020-12-02 21:14:22 -0500

    Day 1, Part 2

--- d01.py
+++ d01.py
@@ -13,7 +13,29 @@ while n[i] + n[j] != target and i < j:
     elif n[i] + n[j] > target:
         j -= 1
 
+print("Part 1")
 if n[i] + n[j] == target:
     print(n[i] * n[j])
+else:
+    print("Nope")
+
+# Part 2
+
+k = 0
+s = n[i] + n[j] + n[k]
+while s != target and k < i and i < j:
+    s = n[i] + n[j] + n[k]
+    if s > target and i + 1 < j:
+        j -= 1
+    elif s > target and i + 1 == j:
+        i -= 1
+    elif s < target and k + 1 < i:
+        k += 1
+    elif s < target and k + 1 == i:
+        i += 1
+
+print("Part 2")
+if s == target:
+    print(n[i] * n[j] * n[k])
 else:
     print("Nope")
