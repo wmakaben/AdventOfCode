246abf76b2625d3b8593aa3658e34ae2bf39e16e
wmakaben <wmakaben@gmail.com>
2020-12-08 07:09:17 -0500

    Day 8, Part 2

--- d08.py
+++ d08.py
@@ -2,21 +2,60 @@ file = open("input/d08.txt", 'r')
 
 code = [line.rstrip().split(" ") for line in file]
 
-accumulator = 0
-visited = set()
-i = 0
+# accumulator = 0
+# visited = set()
+# i = 0
 
-while i not in visited:
-    op = code[i][0]
-    arg = int(code[i][1])
-    visited.add(i)
+# while i not in visited:
+#     op = code[i][0]
+#     arg = int(code[i][1])
+#     visited.add(i)
+#     if op == "acc":
+#         accumulator += arg
+#         i += 1
+#     elif op == "jmp":
+#         i += arg
+#     elif op == "nop":
+#         i += 1
+
+# print(accumulator)
+
+def getAccumulator (idx = 0, visited = set(), switch = True):
+    if idx >= len(code):
+        return 0
+
+    # print(" " * len(visited), code[idx])
+    if idx in visited:
+        return None
+    
+    accumulator = 0
+    visited.add(idx)
+    op = code[idx][0]
+    arg = int(code[idx][1])
+
     if op == "acc":
         accumulator += arg
-        i += 1
+        idx += 1
     elif op == "jmp":
-        i += arg
+        tmpAcc = None
+        if switch:
+            tmpAcc = getAccumulator(idx + 1, set(visited), False)
+        if tmpAcc is None:
+            idx += arg
+        else:
+            return accumulator + tmpAcc
     elif op == "nop":
-        i += 1
+        tmpAcc = None
+        if switch:
+            tmpAcc = getAccumulator(idx + arg, set(visited), False)
+        if tmpAcc is None:
+            idx += 1
+        else:
+            return accumulator + tmpAcc
 
-print(accumulator)
+    nextAcc = getAccumulator(idx, visited, switch)
+    if nextAcc is None:
+        return None
+    return accumulator + nextAcc
 
+print(getAccumulator())
