a9053d0f5f19c90efce2f86c772793b4126b5f35
wmakaben <wmakaben@gmail.com>
2020-12-13 15:55:02 -0500

    WIP Day 13, Part 2

--- d13.py
+++ d13.py
@@ -19,4 +19,36 @@ def partOne(time, buses):
             nextBus = busId
     print("Bus: ", nextBus, ", Wait: ", minWait, ", Answer: ", nextBus * minWait)
 
-partOne(t, b)
+def getBusOffsetMap (buses):
+    offsetMap = {}
+    for i in range(0, len(buses)):
+        if buses[i] == 'x':
+            continue
+        offsetMap[int(buses[i])] = i
+    return offsetMap
+
+def isThatTime (time, buses, offsets):
+    for b in buses:
+        if (time + offsets[b]) % b != 0:
+            return False
+    return True
+
+def partTwo(buses):
+    offsets = getBusOffsetMap(buses)
+    busIds = [int(id) for id in buses if id != 'x']
+
+    maxBusTime = max(busIds)
+    offset = buses.index(str(maxBusTime))
+    print(maxBusTime, offset)
+
+    timeMultiplier = 1
+    # timeMultiplier = 17001991
+    time = (maxBusTime * timeMultiplier) - offset
+    while not isThatTime(time, busIds, offsets):
+        # print(timeMultiplier, time)
+        timeMultiplier += 1
+        time = (maxBusTime * timeMultiplier) - offset
+    print(time)
+
+# partOne(t, b)
+partTwo(b)
